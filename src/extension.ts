d87c7d53c4c94e96ae4b77fec40b798f26dfb020
Daniel Rödl <daniel.roedl@web.de>
2021-03-12 14:46:07 +0100

    changes
    - Add diff to saved version
    - change some messages
    - add handling of untitled files

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,20 @@
 import * as vscode from 'vscode';
 
 import { window, commands } from 'vscode';
-import { existsSync, writeFile, createReadStream, unlink } from 'fs';
+import { existsSync, writeFile, createReadStream, unlink, statSync } from 'fs';
 import * as os from 'os';
-import { join } from 'path';
+import { join, basename } from 'path';
 import * as streamEqual from 'stream-equal';
 import * as cp from 'child_process';
 
 let fillListDone = false;
+let filesToRemove: string[] = [];
+const filesToRemoveGlobal: string[] = [];
+
+export function addFileToRemove(file: string) {
+	filesToRemove.push(file);
+	filesToRemoveGlobal.push(file);
+}
 
 export function showMeld(files: string[]) {
 	const diffTool = vscode.workspace.getConfiguration('meld-diff').diffCommand;
@@ -48,7 +55,7 @@ export function showMeld(files: string[]) {
 		});
 }
 
-export function showListAndDiff(current: string, possible_diffs: string[]) {
+export function showListAndDiff(current: string, possible_diffs: string[], filesToRemove: string[]) {
 	// remove current editor
 	const possible = possible_diffs.filter(function (value, index, arr) {
 		return value != current;
@@ -63,15 +70,19 @@ export function showListAndDiff(current: string, possible_diffs: string[]) {
 		placeHolder: 'Filename to diff'
 	}).then(result => {
 		if (existsSync(result)) {
-			showMeld([current, result]);
+			const process = showMeld([current, result]);
+			if (process && filesToRemove.length > 0) {
+				const files = [...filesToRemove];
+				process.on('exit', () => cleanupTmpFiles(files));
+			}
 		}
 	});
 }
 
 // workaround because there is no function to get all open editors from API
-export function doIt(current: string, possible_diffs: string[]) {
+export function doIt(current: string, possible_diffs: string[], filesToRemove: string[]) {
 	if (fillListDone) {
-		showListAndDiff(current, possible_diffs);
+		showListAndDiff(current, possible_diffs, filesToRemove);
 	} else {
 		if (window.activeTextEditor) {
 			possible_diffs.push(window.activeTextEditor.document.fileName);
@@ -79,19 +90,18 @@ export function doIt(current: string, possible_diffs: string[]) {
 		commands.executeCommand("workbench.action.nextEditor").then(_ => {
 			if (window.activeTextEditor) {
 				if (window.activeTextEditor.document.fileName != current) {
-					doIt(current, possible_diffs);
+					doIt(current, possible_diffs, filesToRemove);
 				} else {
 					fillListDone = true;
-					showListAndDiff(current, possible_diffs);
+					showListAndDiff(current, possible_diffs, filesToRemove);
 				}
 			} else {
 				// the window is not a text editor, skip it
-				doIt(current, possible_diffs);
+				doIt(current, possible_diffs, filesToRemove);
 			}
 		});
 	}
 }
-
 
 function rndName() {
 	return Math.random().toString(36).substr(2, 10);
@@ -115,8 +125,8 @@ export function createRandomFile({ contents = '', prefix = 'tmp' }: { contents?:
 	});
 }
 
-export async function writeFileOnDisk(content: string): Promise<string> {
-	return (await createRandomFile({ contents: content })).fsPath;
+export async function writeTempFileOnDisk(content: string, prefix: string = "tmp_"): Promise<string> {
+	return (await createRandomFile({ contents: content, prefix: prefix })).fsPath;
 }
 
 export async function areFilesEqual(files: string[]): Promise<boolean> {
@@ -132,7 +142,27 @@ export function cleanupTmpFiles(files: string[]) {
 		if (err) {
 			console.log('Unable to delete file: ', file);
 		}
+		// remove entry from global list
+		const index = filesToRemoveGlobal.indexOf(file);
+		if (index > -1) {
+			filesToRemoveGlobal.splice(index);
+		}
 	}));
+}
+
+export async function getFileNameOfDocument(document: vscode.TextDocument) {
+	if (document.isUntitled) {
+		//compare untitled file or changed content of file instead of saved file
+		let prefix = "untitled_";
+		if (! document.isUntitled) {
+			prefix = basename(document.fileName) + "_";
+		}
+		const documentContent = document.getText();
+		const fileName = await writeTempFileOnDisk(documentContent, prefix);
+		return { name: fileName, tmp: true };
+	} else {
+		return { name: document.fileName, tmp: false };
+	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -155,46 +185,73 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffVisible', () => {
-		const open_files: string[] = [];
-		window.visibleTextEditors.forEach(editor => {
-			// TODO prüfen editor.document.isDirty ... wenn dirty, dann statt file ein tmp file mit 'editor.document.getText()' verwenden
-			// TODO in settings einstellbar machen was verwendet werden soll
-			open_files.push(editor.document.fileName.toString());
-		});
+	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffVisible', async () => {
+		let open_files: string[] = [];
+		filesToRemove = [];
+
+		for (const editor of window.visibleTextEditors) {
+			const fileName = await getFileNameOfDocument(editor.document);
+			open_files.push(fileName.name);
+			if (fileName.tmp) {
+				addFileToRemove(fileName.name);
+			}
+		}
 
 		if (open_files.length < 2) {
-			window.showErrorMessage("Meld Diff:\nCan't diff: Only one file is visible in editor!");
+			let fileCount = "Only one file is";
+			if (open_files.length == 0) {
+				fileCount = "No files are";
+			}
+			window.showErrorMessage("Can't compare! " + fileCount + " visible in editor!");
 			return;
 		}
 
-		showMeld(open_files);
+		// sort open files by last modification, newest first
+		open_files = open_files.map(function (fileName) {
+			return {
+			  name: fileName,
+			  time: statSync(fileName).mtime.getTime()
+			};
+		  })
+		  .sort(function (a, b) {
+			return b.time - a.time; })
+		  .map(function (v) {
+			return v.name; });
+
+		// TODO add areFilesEqual to every step
+		const process = showMeld(open_files);
+		if (process && filesToRemove.length > 0) {
+			const files = [...filesToRemove];
+			process.on('exit', () => cleanupTmpFiles(files));
+		}
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffCurrentToOtherOpen', () => {
-		const open_files: string[] = [];
-
+	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffCurrentToOtherOpen', async () => {
 		if (!window.activeTextEditor) {
-			window.showErrorMessage("Meld Diff:\nCurrent window is not a file!");
+			window.showErrorMessage("Current window is not an editor!");
 			return;
 		}
 
-		// add active file
-		open_files.push(window.activeTextEditor.document.fileName);
+		filesToRemove = [];
+		const current = await getFileNameOfDocument(window.activeTextEditor.document);
+		if (current.tmp) {
+			addFileToRemove(current.name);
+		}
 
-		// let possible_diffs= Array();
-		const current = window.activeTextEditor.document.fileName;
-
-		doIt(current, open_files_event);
+		doIt(current.name, open_files_event, filesToRemove);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffCurrentToOther', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffCurrentToOther', async () => {
 		if (!window.activeTextEditor) {
-			window.showErrorMessage("Meld Diff:\nNo file selected for diff!");
+			window.showErrorMessage("Current window is not an editor!")
 			return;
 		}
 
-		const current = window.activeTextEditor.document.fileName;
+		filesToRemove = [];
+		const current = await getFileNameOfDocument(window.activeTextEditor.document);
+		if (current.tmp) {
+			addFileToRemove(current.name);
+		}
 
 		const options: vscode.OpenDialogOptions = {
 			canSelectMany: false,
@@ -203,7 +260,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 		window.showOpenDialog(options).then(_ => {
 			if (_) {
-				showMeld([current, _[0].fsPath]);
+				const process = showMeld([current.name, _[0].fsPath]);
+				if (process && filesToRemove.length > 0) {
+					const files = [...filesToRemove];
+					process.on('exit', () => cleanupTmpFiles(files));
+				}
 			}
 		});
 
@@ -212,14 +273,14 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffWithClipboard', async () => {
 		const editor = window.activeTextEditor;
 		if (!editor) {
-			window.showErrorMessage("Meld Diff:\nCurrent window is not a file!");
+			window.showErrorMessage("Current window is not an editor!");
 			return;
 		}
 
-		const filesToRemove: string[] = [];
+		filesToRemove = [];
 		const clipboardContent = await vscode.env.clipboard.readText();
-		const clipboard = await writeFileOnDisk(clipboardContent);
-		filesToRemove.push(clipboard);
+		const clipboard = await writeTempFileOnDisk(clipboardContent, "clipboard_");
+		addFileToRemove(clipboard);
 
 		//by default compare clipboard against current file
 		let sameContent;
@@ -228,20 +289,20 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!selection.isEmpty) {
 			//compare against current selection
 			const editorContent = editor.document.getText(selection);
-			current = await writeFileOnDisk(editorContent);
-			filesToRemove.push(current);
+			current = await writeTempFileOnDisk(editorContent, "selection_");
+			addFileToRemove(current);
 			sameContent = await areFilesEqual([current, clipboard]);
 		} else if (editor.document.isUntitled) {
 			//compare against untitled file
 			const editorContent = editor.document.getText();
-			current = await writeFileOnDisk(editorContent);
-			filesToRemove.push(current);
+			current = await writeTempFileOnDisk(editorContent, "untitled_");
+			addFileToRemove(current);
 			sameContent = await areFilesEqual([current, clipboard]);
 		} else if (editor.document.isDirty) {
 			//compore against dirty content but invoke meld with current saved file
 			const editorContent = editor.document.getText();
-			const tmpCheck = await writeFileOnDisk(editorContent);
-			filesToRemove.push(tmpCheck);
+			const tmpCheck = await writeTempFileOnDisk(editorContent);
+			addFileToRemove(tmpCheck);
 			sameContent = await areFilesEqual([tmpCheck, clipboard]);
 		} else {
 			sameContent = await areFilesEqual([current, clipboard]);
@@ -252,7 +313,47 @@ export function activate(context: vscode.ExtensionContext) {
 			cleanupTmpFiles(filesToRemove);
 		} else {
 			const process = showMeld([current, clipboard]);
-			process.on('exit', () => cleanupTmpFiles(filesToRemove));
+			if (process && filesToRemove.length > 0) {
+				const files = [...filesToRemove];
+				process.on('exit', () => cleanupTmpFiles(files));
+			}
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffSavedVersion', async () => {
+		const editor = window.activeTextEditor;
+		if (!editor) {
+			window.showErrorMessage("Current window is not an editor!");
+			return;
+		}
+
+		if (editor.document.isUntitled) {
+			window.showErrorMessage("No saved version found to compare with!");
+			return;
+		}
+
+		if (!editor.document.isDirty) {
+			window.showInformationMessage("No difference to saved version of the file.");
+			return;
+		}
+
+		filesToRemove = [];
+
+		const editorContent = editor.document.getText();
+		const currentSaved = editor.document.fileName;
+		const current = await writeTempFileOnDisk(editorContent, basename(currentSaved)+"_changed_");
+		addFileToRemove(current);
+		const sameContent = await areFilesEqual([current, currentSaved]);
+
+		if (sameContent) {
+			window.showInformationMessage('No difference to saved version of the file.');
+			cleanupTmpFiles(filesToRemove);
+		} else {
+			const process = showMeld([current, currentSaved]);
+			if (process && filesToRemove.length > 0) {
+				const files = [...filesToRemove];
+				process.on('exit', () => cleanupTmpFiles(files));
+			}
 		}
 	}));
 
@@ -261,8 +362,13 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffFromFileListSelect', (_) => {
 		if (!_) {
 			if (window.activeTextEditor) {
+				if (window.activeTextEditor.document.isUntitled) {
+					window.showErrorMessage("Unsaved editors can not be selected for meld diff comparison!");
+					return;
+				}
 				selected = window.activeTextEditor.document.fileName;
 			} else {
+				window.showErrorMessage("Current window is not an editor!");
 				return;
 			}
 		} else {
@@ -271,25 +377,43 @@ export function activate(context: vscode.ExtensionContext) {
 		console.log("Select for meld compare: " + selected);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffFromFileList', (_) => {
+	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffFromFileList', async (_) => {
 		let path = "";
+		filesToRemove = [];
+
 		if (!_) {
 			if (window.activeTextEditor) {
-				path = window.activeTextEditor.document.fileName;
+				let fileName = await getFileNameOfDocument(window.activeTextEditor.document);
+				if (fileName.tmp) {
+					addFileToRemove(fileName.name);
+				}
+				path = fileName.name;
 			} else {
+				window.showErrorMessage("Current window is not an editor!");
 				return;
 			}
 		} else {
 			path = _.fsPath;
 		}
 		if (selected.length > 0) {
-			showMeld([selected, path]);
+			const process = showMeld([selected, path]);
+			if (process && filesToRemove.length > 0) {
+				const files = [...filesToRemove];
+				process.on('exit', () => cleanupTmpFiles(files));
+			}
 		} else {
-			window.showErrorMessage("Meld Diff: First select a file to compare to!");
+			window.showErrorMessage("First select a file to compare with!");
 		}
 	}));
 }
 
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export function deactivate() { }
+export function deactivate() {
+	// delete all tmp files that are not yet deleted because vscode is closed before meld
+	if (vscode.workspace.getConfiguration('meld-diff').cleanUpTempFilesOnCodeClose) {
+		filesToRemoveGlobal.forEach((file) => unlink(file, (err) => {
+			if (err) {
+				console.log('Unable to delete file: ', file);
+			}
+		}));
+	}
+}
