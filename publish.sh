3cf43890fc78830d14f1dbbe8234bb85f9b6561f
Daniel Rödl <daniel.roedl@web.de>
2023-06-09 16:03:26 +0200

    Add publisher for open-vsx.org

--- publish.sh
+++ publish.sh
@@ -24,15 +24,23 @@ if [ $(vsce ls-publishers | grep -ce "danielroedl") -ne 1 ]; then
 else
     echo "> User logged in"
 fi
+[[ -z $OVSX_TOKEN ]] && { echo "Please define OVSX_TOKEN first to publish also to open-vsx.org!"; exit 1; }
 echo
 echo "> Run package"
 echo
 vsce package
 echo
-echo "Start publishing? [Enter to continue]"
+echo "Start publishing to marketplace.visualstudio.com? [Enter to continue]"
 read
 echo
 vsce publish
+echo
+echo "> Publishing done"
+echo
+echo "Start packaging and publishing to open-vsx.org? [Enter to continue]"
+read
+echo
+npx ovsx publish -p $OVSX_TOKEN
 echo
 echo "> Publishing done"
 echo
